6c558913030422d9440f9118f78edf8b2921cb44
Matthew Vandenberg <mv5903@gmail.com>
2022-04-26 09:45:33 -0700

    JUnit Tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,26 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1+1);
+    }
+
+    @Test
+    public void testGetLinks() {
+        try {
+            List<String> actual = MarkdownParse.getLinks(new Scanner(new File("test-file.md")));
+            List<String> expected = List.of("https://something.com", "some-thing.html");
+            assertEquals(expected, actual);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
