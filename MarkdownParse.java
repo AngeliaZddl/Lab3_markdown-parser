55cb30dd4a5aee31b8a5761fde32f1e0fff1f9f4
Matthew Vandenberg <mv5903@gmail.com>
2022-05-12 11:20:19 -0700

    Add comments and remove code

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,28 +16,28 @@ public class MarkdownParse {
                 if (currentLine.length() == 0) continue;
                 // Ensure that line isn't an image
                 if (currentLine.charAt(0) == '!') continue;
+                // Let's make sure all characters ('[', ']', '(', ')') exist in string
                 int counter = 0;
                 for (String character : VALID_CHARS) {
                     if (currentLine.contains(character)) {
                         counter++;
                     }
                 }
+                // If we don't find all the characters, we know this line doesn't contain a link
                 if (counter != 4) break;
-                // Found all characters that are needed to consider this line a link
-                if (counter == 4) {
-                    // Make sure that the link characters are in the correct order
-                    int startSquareBracket = currentLine.indexOf("[");
-                    if (startSquareBracket == -1) continue;
-                    int endSquareBracket = currentLine.indexOf("]", startSquareBracket);
-                    if (endSquareBracket == -1) continue;
-                    int startParenthesis = currentLine.indexOf("(", endSquareBracket);
-                    if (startParenthesis == -1) continue;
-                    int endParenthesis = currentLine.indexOf(")", startParenthesis);
-                    if (endParenthesis == -1) continue;
-                    String link = currentLine.substring(startParenthesis + 1, endParenthesis);
-                    list.add(link);
-                    currentLine = currentLine.substring(endParenthesis + 1);
-                }
+                // Make sure that the link characters are in the correct order
+                int startSquareBracket = currentLine.indexOf("[");
+                if (startSquareBracket == -1) continue;
+                int endSquareBracket = currentLine.indexOf("]", startSquareBracket);
+                if (endSquareBracket == -1) continue;
+                int startParenthesis = currentLine.indexOf("(", endSquareBracket);
+                if (startParenthesis == -1) continue;
+                int endParenthesis = currentLine.indexOf(")", startParenthesis);
+                if (endParenthesis == -1) continue;
+                String link = currentLine.substring(startParenthesis + 1, endParenthesis);
+                list.add(link);
+                currentLine = currentLine.substring(endParenthesis + 1);
+                
             } while (currentLine.length() != 0);
         }
         return list;
